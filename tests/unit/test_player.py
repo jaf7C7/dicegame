a82eadc140947d20ce21c2ee25f9c9f094cb1688
Joss <joss@WS.home>
2024-03-16 16:03:18 +0000

    Refactor tests into classes

--- tests/unit/test_player.py
+++ tests/unit/test_player.py
@@ -1,21 +1,26 @@
 from player import Player
 
 
-def test_has_die_attribute():
-    assert hasattr(Player(), 'die')
+class TestDie:
+    def test_has_die_attribute(self):
+        assert hasattr(Player(), 'die')
 
-def test_die_attribute_is_die_instance():
-    from die import Die
-    assert isinstance(Player().die, Die)
+    def test_die_attribute_is_die_instance(self):
+        from die import Die
+        assert isinstance(Player().die, Die)
 
-def test_has_is_cpu_attribute():
-    assert hasattr(Player(), 'is_cpu')
 
-def test_is_cpu_attr_is_boolean():
-    assert isinstance(Player().is_cpu, bool)
+class TestIsCPU:
+    def test_has_is_cpu_attribute(self):
+        assert hasattr(Player(), 'is_cpu')
 
-def test_has_counter_attribute():
-    assert hasattr(Player(), 'counter')
+    def test_is_cpu_attr_is_boolean(self):
+        assert isinstance(Player().is_cpu, bool)
 
-def test_counter_initialised_to_10():
-    assert Player().counter == 10
+
+class TestCounter:
+    def test_has_counter_attribute(self):
+        assert hasattr(Player(), 'counter')
+
+    def test_counter_initialised_to_10(self):
+        assert Player().counter == 10
