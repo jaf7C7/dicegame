1e99110d1e9946c1d8de721ad81f3a8a7873e32a
Joss <joss@WS.home>
2024-03-18 18:39:17 +0000

    Refactor tests

--- tests/unit/test_die.py
+++ tests/unit/test_die.py
@@ -2,25 +2,22 @@ import pytest
 from die import Die
 
 
-def test_has_value_attribute():
-    assert hasattr(Die(), 'value')
+class TestDie:
+    def test_has_value_attribute(self):
+        assert hasattr(Die(), 'value')
 
+    def test_value_initialised_to_none(self):
+        assert Die().value is None
 
-def test_value_initialised_to_none():
-    assert Die().value is None
+    def test_value_cannot_be_changed_outside_the_class(self):
+        with pytest.raises(AttributeError):
+            Die().value = 10
 
+    def test_has_roll_method(self):
+        assert hasattr(Die(), 'roll')
 
-def test_value_cannot_be_changed_outside_the_class():
-    with pytest.raises(AttributeError):
-        Die().value = 10
-
-
-def test_has_roll_method():
-    assert hasattr(Die(), 'roll')
-
-
-def test_roll_generates_random_numbers_between_1_and_6():
-    die = Die()
-    for i in range(2):
-        die.roll()
-        assert die.value in range(1, 7)
+    def test_roll_generates_random_numbers_between_1_and_6(self):
+        die = Die()
+        for i in range(2):
+            die.roll()
+            assert die.value in range(1, 7)
