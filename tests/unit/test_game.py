ce6302634c38e5acaebd8456811eaf121aff5398
Joss <joss@WS.home>
2024-03-18 19:12:55 +0000

    Refactor tests

--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -3,9 +3,13 @@ from unittest.mock import Mock
 from game import Game
 
 
+@pytest.fixture
+def game():
+    return Game(player_1=Mock(), player_2=Mock(), display=Mock())
+
+
 class TestPlay:
-    def test_displays_welcome_message(self):
-        game = Game(player_1=Mock(), player_2=Mock(), display=Mock())
+    def test_displays_welcome_message(self, game):
         game.play()
         game.display.assert_any_call(
             '=========================\n'
@@ -15,15 +19,13 @@ class TestPlay:
 
 
 class TestPlayRound:
-    def test_displays_round_start_message(self):
-        game = Game(player_1=Mock(), player_2=Mock(), display=Mock())
+    def test_displays_round_start_message(self, game):
         game.play_round()
         game.display.assert_any_call(
             'Round 1:\n'
             '--------\n'
         )  # fmt: skip
 
-    def test_calls_roll_die_on_players(self):
-        game = Game(player_1=Mock(), player_2=Mock(), display=Mock())
+    def test_calls_roll_die_on_players(self, game):
         game.play_round()
         assert all(p.roll_die.called for p in (game.player_1, game.player_2))
