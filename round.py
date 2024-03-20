d8ebe1a45df6112b4d218f2c36e2db26bf4594ed
Joss <joss@WS.home>
2024-03-20 19:25:40 +0000

    Add `player_1` and `player_2` attributes to `Round`

--- round.py
+++ round.py
@@ -1,6 +1,8 @@
 class Round:
 
-    def __init__(self, number, display):
+    def __init__(self, player_1=None, player_2=None, number=1, display=print):
+        self.player_1 = player_1
+        self.player_2 = player_2
         self.number = number
         self.display = display
 
