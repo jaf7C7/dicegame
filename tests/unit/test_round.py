7aa31692397f42b4582e0a5370906f0683315a0d
Joss <joss@WS.home>
2024-03-21 11:35:55 +0000

    Refactoring

--- tests/unit/test_round.py
+++ tests/unit/test_round.py
@@ -1,13 +1,16 @@
 from unittest.mock import Mock
 from round import Round
 
+
 class TestPlayers:
+
     def test_has_two_players(self):
         round_ = Round(player_1=Mock(), player_2=Mock())
         assert hasattr(round_, 'player_1') and hasattr(round_, 'player_2')
 
 
 class TestPlay:
+
     def test_displays_correct_round_number(self):
         round_ = Round(display=Mock(), number=2)
         round_.play_round()
