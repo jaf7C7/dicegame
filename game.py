c5719c24d205b56bb60e0947c8d8a6ab8a0a8d89
Joss <joss@WS.home>
2024-03-22 13:09:10 +0000

    Extract `Round` class from `Game`

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from round import Round
 from player import Player
 
 
@@ -5,34 +6,32 @@ class Game:
     """A game to be played by two players over a number of rounds"""
 
     def __init__(
-        self, player_1=Player(), player_2=Player(is_cpu=True), display=print
+        self,
+        player_1=Player(),
+        player_2=Player(is_cpu=True),
+        display=print,
+        round_=None,
     ):
         self.player_1 = player_1
         self.player_2 = player_2
         self.display = display
-        self.round_number = 0
+        if round_ is None:
+            round_ = Round(
+                self.player_1,
+                self.player_2,
+                display=self.display,
+                input_=input,
+            )
+        self.round = round_
         self.game_winner = None
 
     def play(self):
         self._display_game_welcome_message()
 
         while not self._game_over():
-            self.play_round()
+            self.round.play()
 
         self._display_game_over_message()
-
-    def play_round(self):
-        self.round_number += 1
-        self._display_round_welcome_message()
-
-        for player in (self.player_1, self.player_2):
-            if not player.is_cpu:
-                input('Press any key to roll your die... ')
-            player.roll_die()
-
-        self._display_die_values()
-        self._set_round_winner()
-        self._display_player_counters()
 
     def _game_over(self):
         self._set_game_winner()
@@ -61,42 +60,5 @@ class Game:
             '\n'
             'And the winner is...\n'
             f'{self.game_winner}!\n'
-            '\n'
-        )
-
-    def _display_round_welcome_message(self):
-        self.display(
-            f'Round {self.round_number}:\n'
-            '--------\n'
-        )  # fmt: skip
-
-    def _display_die_values(self):
-        self.display(
-            '\n'
-            f'Player 1 rolled: {self.player_1.die.value}\n'
-            f'Player 2 rolled: {self.player_2.die.value}\n'
-        )
-
-    def _set_round_winner(self):
-        if self.player_1.die.value == self.player_2.die.value:
-            self.round_result = "It's a Tie!"
-        elif self.player_1.die.value > self.player_2.die.value:
-            self.round_result = 'WINNER: Player 1'
-            self.player_1.decrement_counter()
-            self.player_2.increment_counter()
-        else:
-            self.round_result = 'WINNER: Player 2'
-            self.player_2.decrement_counter()
-            self.player_1.increment_counter()
-
-    def _display_player_counters(self):
-        self.display(
-            '*************************\n'
-            f'Round {self.round_number}: {self.round_result}\n'
-            '*************************\n'
-            '\n'
-            '~~~~ Player counters: ~~~~\n'
-            f'Player 1: {self.player_1.counter}\n'
-            f'Player 2: {self.player_2.counter}\n'
             '\n'
         )
--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -8,6 +8,7 @@ def game():
     return Game(
         player_1=Mock(is_cpu=False, counter=5, die=Mock(value=0)),
         player_2=Mock(is_cpu=True, counter=5, die=Mock(value=0)),
+        round_=Mock(),
         display=Mock(),
     )
 
@@ -39,90 +40,9 @@ class TestPlay:
     def test_calls_play_round_until_game_over(self, game):
         with (
             patch.object(game, '_game_over', side_effect=[False, False, True]),
-            patch.object(game, 'play_round') as fake_play_round,
         ):
             game.play()
-            assert fake_play_round.call_count == 2
-
-
-class TestPlayRound:
-
-    def test_displays_incrementing_round_numbers(self, game):
-        with patch('game.input', return_value=''):
-            game.play_round()
-            game.display.assert_any_call(
-                'Round 1:\n'
-                '--------\n'
-            )  # fmt: skip
-
-            game.play_round()
-            game.display.assert_any_call(
-                'Round 2:\n'
-                '--------\n'
-            )  # fmt: skip
-
-    def test_requires_input_from_human_player_to_roll(self, game):
-        with patch('game.input', return_value='') as fake_input:
-            game.play_round()
-            fake_input.assert_called_once_with(
-                'Press any key to roll your die... '
-            )
-
-    def test_calls_roll_die_on_players(self, game):
-        with patch('game.input', return_value='') as fake_input:
-            game.play_round()
-        assert all(p.roll_die.called for p in (game.player_1, game.player_2))
-
-    def test_displays_results_of_each_die_roll(self, game):
-        with patch('game.input', return_value=''):
-            game.play_round()
-            game.display.assert_any_call(
-                '\n'
-                f'Player 1 rolled: {game.player_1.die.value}\n'
-                f'Player 2 rolled: {game.player_2.die.value}\n'
-            )
-
-    def test_calls_update_counter_methods_on_players(self, game):
-        with patch('game.input', return_value=''):
-            game.player_1.die.value = 6
-            game.player_2.die.value = 1
-            game.play_round()
-            game.player_1.decrement_counter.assert_called()
-            game.player_2.increment_counter.assert_called()
-
-    def test_update_counter_methods_not_called_if_round_tied(self, game):
-        with patch('game.input', return_value=''):
-            game.player_1.die.value = 1
-            game.player_2.die.value = 1
-            game.play_round()
-            game.player_1.decrement_counter.assert_not_called()
-            game.player_1.increment_counter.assert_not_called()
-            game.player_2.decrement_counter.assert_not_called()
-            game.player_2.increment_counter.assert_not_called()
-
-    @pytest.mark.parametrize(
-        'p1_die,p2_die,result',
-        [
-            ('6', '1', 'WINNER: Player 1'),
-            ('1', '6', 'WINNER: Player 2'),
-            ('1', '1', "It's a Tie!"),
-        ],
-    )
-    def test_displays_results_and_counters(self, game, p1_die, p2_die, result):
-        with patch('game.input', return_value=''):
-            game.player_1.die.value = p1_die
-            game.player_2.die.value = p2_die
-            game.play_round()
-            game.display.assert_any_call(
-                '*************************\n'
-                f'Round {game.round_number}: {result}\n'
-                '*************************\n'
-                '\n'
-                '~~~~ Player counters: ~~~~\n'
-                f'Player 1: {game.player_1.counter}\n'
-                f'Player 2: {game.player_2.counter}\n'
-                '\n'
-            )
+            assert game.round.play.call_count == 2
 
 
 class TestGameOver:
