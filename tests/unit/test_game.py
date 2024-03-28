1bd3b16e285cc6d1671ff5025d4a1bca3eabe373
Joss <joss@WS.home>
2024-03-28 12:15:13 +0000

    Refactoring

--- tests/unit/test_game.py
+++ tests/unit/test_game.py
@@ -6,39 +6,36 @@ from game import Game
 @pytest.fixture
 def game():
     game = Game(round_class=Mock, ui=Mock(), player_class=Mock)
-    game.add_player()
-    game.add_player()
     return game
 
 
 class TestPlayers:
 
-    def test_fails_if_has_less_than_two_players(self):
-        game = Game(round_class=Mock, ui=Mock(), player_class=Mock)
+    def test_fails_if_has_less_than_two_players(self, game):
         game.add_player()
         with pytest.raises(AttributeError):
             game.play()
 
-    def test_add_player_increases_number_of_players(self):
-        game = Game(round_class=Mock, ui=Mock(), player_class=Mock)
-        assert len(game.players) == 0
+    def test_add_player_increases_number_of_players(self, game):
         game.add_player()
         assert len(game.players) == 1
 
-    def test_add_player_sets_player_number(self):
-        game = Game(round_class=Mock, ui=Mock(), player_class=Mock)
-        assert len(game.players) == 0
+    def test_add_player_sets_player_number(self, game):
         game.add_player()
         assert game.players[0].number == 1
 
-    def test_add_player_can_add_cpu_player(self):
-        game = Game(round_class=Mock, ui=Mock(), player_class=Mock)
-        assert len(game.players) == 0
+    def test_add_player_can_add_cpu_player(self, game):
         game.add_player(is_cpu=True)
         assert game.players[0].is_cpu is True
 
 
 class TestPlay:
+
+    @pytest.fixture
+    def game(self, game):
+        game.add_player()
+        game.add_player()
+        return game
 
     def test_displays_welcome_message(self, game):
         game.players[0].counter = 0
@@ -99,11 +96,21 @@ class TestPlay:
 
 class TestGameOver:
 
-    def test_returns_true_when_any_player_counter_is_zero(self, game):
+    @pytest.fixture
+    def game(self, game):
         game.add_player()
-        game.players[2].counter = 0
+        game.add_player()
+        return game
+
+    @pytest.mark.parametrize('p1_counter,p2_counter', ((0, 1), (1, 0)))
+    def test_returns_true_when_any_player_counter_is_zero(
+        self, game, p1_counter, p2_counter
+    ):
+        game.players[0].counter = p1_counter
+        game.players[1].counter = p2_counter
         assert game.game_over() is True
 
     def test_returns_false_when_neither_player_counter_is_zero(self, game):
-        assert all(p.counter != 0 for p in game.players)
+        game.players[0].counter = 1
+        game.players[1].counter = 2
         assert game.game_over() is False
